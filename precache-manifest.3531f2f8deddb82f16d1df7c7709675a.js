72c6510e2c1434ed1bdb666940003fc77ab51729
irvin pineda <62679993+irvinpinedaUCA@users.noreply.github.com>
2021-11-29 13:11:05 -0600

    Updates

---
+++ precache-manifest.3531f2f8deddb82f16d1df7c7709675a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1adacd316fd2c217fb5c4c4c9628749f",
+    "url": "/JINBook/index.html"
+  },
+  {
+    "revision": "ac22beffc1a18451580b",
+    "url": "/JINBook/static/css/main.b91fb80a.chunk.css"
+  },
+  {
+    "revision": "b394c1143b3a921620b5",
+    "url": "/JINBook/static/js/2.f2dcd7ef.chunk.js"
+  },
+  {
+    "revision": "0569f079e111a6d4f0bf8418a634cb07",
+    "url": "/JINBook/static/js/2.f2dcd7ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31e23b798db19b14ba16",
+    "url": "/JINBook/static/js/3.76126ac3.chunk.js"
+  },
+  {
+    "revision": "ac22beffc1a18451580b",
+    "url": "/JINBook/static/js/main.f18a0d50.chunk.js"
+  },
+  {
+    "revision": "652ab151aa477558a46c",
+    "url": "/JINBook/static/js/runtime-main.f4d8c48b.js"
+  },
+  {
+    "revision": "49c53331d19be74b52d47fcce7e97468",
+    "url": "/JINBook/static/media/programmer.49c53331.png"
+  }
+]);
